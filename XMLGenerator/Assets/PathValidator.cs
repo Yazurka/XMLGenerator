9af115d5834559f5677328ac23a50312bf89f03b
Frederik Tegnander <frederik.t91@gmail.com>
2015-11-12 22:08:06 +0100

    Endret hvordan PathValidator fungerer

--- XMLGenerator/Assets/PathValidator.cs
+++ XMLGenerator/Assets/PathValidator.cs
@@ -1,24 +1,73 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace XMLGenerator.Assets
 {
     public static class PathValidator
     {
-        public static bool ValidatePath(string BaseFolderPath, string SelectedPath)
+        private static bool Cancel = false;
+
+        private static bool ValidatePath(string BaseFolderPath, string SelectedPath)
         {
-            var IsValid = false;
+            if (string.IsNullOrEmpty(BaseFolderPath))
+            {
+                return false;
+            }
 
             if (SelectedPath.Substring(BaseFolderPath.Length) == BaseFolderPath)
             {
-                IsValid = true;
-            }           
-            
+                return true;
+            }
 
-            return IsValid;
+            return false;
         }
+
+        public static string SelectPath(string BaseFolderPath)
+        {
+            var returnString = string.Empty;
+
+            FolderBrowserDialog p = new FolderBrowserDialog();
+            p.ShowDialog();
+            var selectedPath = p.SelectedPath;
+            var isValid = PathValidator.ValidatePath(BaseFolderPath, selectedPath);
+            while (PathValidator.ValidatePath(BaseFolderPath, selectedPath) && !Cancel)
+            {
+                if (PathValidator.ValidatePath(BaseFolderPath, selectedPath))
+                {
+                    returnString = p.SelectedPath;
+                    break;
+                }
+                else
+                {
+                    Message(BaseFolderPath, p.SelectedPath);
+                }        
+            }
+            return p.SelectedPath;
+        }
+
+        private static async void Message(string BaseFolderPath, string SelectedPath)
+        {
+            var window = System.Windows.Application.Current.MainWindow as MetroWindow;
+            MetroDialogSettings Settings = new MetroDialogSettings();
+            Settings.AffirmativeButtonText = "Yes";
+            Settings.NegativeButtonText = "No";
+            var x = await window.ShowMessageAsync("Not allowed", "The selected path is invalid, do you want to try again?", MessageDialogStyle.AffirmativeAndNegative, Settings);
+
+            switch (x)
+            {
+                case MessageDialogResult.Negative:
+                    Cancel = true;
+                    break;
+                case MessageDialogResult.Affirmative:                    
+                    break;
+            }
+        }
+
     }
 }
--- XMLGenerator/Model/File.cs
+++ XMLGenerator/Model/File.cs
@@ -8,11 +8,14 @@ using System.Windows.Input;
 using System.Windows.Media;
 using Microsoft.Practices.Prism.Commands;
 using XMLGenerator.ViewModel;
+using XMLGenerator.Assets;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
 
 namespace XMLGenerator.Model
 {
     public class File : ViewModelBase
-    {   
+    {
         private bool m_isVisible;
         private string m_from;
         private ICommand m_fileDialogCommand;
@@ -25,16 +28,42 @@ namespace XMLGenerator.Model
 
         private void setPath()
         {
-            FolderBrowserDialog p = new FolderBrowserDialog();
-            p.ShowDialog();
-            From = p.SelectedPath;
+            From = PathValidator.SelectPath(FromRestriction);
+
+            //FolderBrowserDialog p = new FolderBrowserDialog();
+            //p.ShowDialog();
+            //var selectedPath = p.SelectedPath;
+            //var isValid = PathValidator.ValidatePath(FromRestriction, selectedPath);
+            //if (isValid)
+            //{
+            //    From = p.SelectedPath;
+            //}
+            //else
+            //{
+            //    var window = System.Windows.Application.Current.MainWindow as MetroWindow;
+            //    MetroDialogSettings Settings = new MetroDialogSettings();
+            //    Settings.AffirmativeButtonText = "Yes";
+            //    Settings.NegativeButtonText = "No";
+            //    var x = await window.ShowMessageAsync("Not allowed", "The selected path is invalid, do you want to try again?", MessageDialogStyle.AffirmativeAndNegative, Settings);
+
+            //    switch (x)
+            //    {
+            //        case MessageDialogResult.Negative:
+            //            return;
+            //        case MessageDialogResult.Affirmative:
+            //            setPath();
+            //            break;
+            //    }
+            //}
         }
-         private ICommand m_removeCommand;
-        
+
+        private ICommand m_removeCommand;
+
         private void RemoveExecute()
         {
             IsVisible = false;
         }
+
         public ICommand RemoveCommand { get { return m_removeCommand; } set { m_removeCommand = value; OnPropertyChanged("RemoveCommand"); } }
         public string FromRestriction { get; set; }
         public bool IsVisible { get { return m_isVisible; } set { m_isVisible = value; OnPropertyChanged("IsVisible"); } }
